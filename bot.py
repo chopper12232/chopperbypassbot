58c8375daf7ef3b64b555897f9e5c03fcd8dec31
chopper12232 <narekvirek23@outlook.de>
2026-08-07 17:44:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 PHONE = os.environ.get("PHONE", "")
 
-OTHER_BOT = "robloxbypass"  # Юзернейм чужого бота
+OTHER_BOT = "getkey"  # Юзернейм чужого бота
 pending_requests = []
 
 user_client = TelegramClient("user_session", API_ID, API_HASH)
